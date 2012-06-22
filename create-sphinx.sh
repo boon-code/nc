abf300f2a6413020198a58b4a5383812e0fb002b
Manuel Huber <Manuel.h87@gmail.com>
2012-06-22 22:47:20 +0200

    Now excluding .buildinfo from documentation.tgz

--- create-sphinx.sh
+++ create-sphinx.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)
 mkdir -p ./sphinx/build/html
 touch ./sphinx/build/html/.nojekyll
 make --directory=sphinx/ html
-tar cz --directory=./sphinx/build/html/ . > documentation.tgz
+tar cz --directory=./sphinx/build/html/ --exclude=.buildinfo . > documentation.tgz
 
